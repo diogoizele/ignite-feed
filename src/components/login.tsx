8a37a5c127ef6b21e4a4cd7420a85cc17c27e5ad
Diogo Izele <diogogabrielizele@gmail.com>
2022-06-24 15:15:13 -0300

    feat(login): creates login HOC and user render props inside

---
+++ src/components/login.tsx
@@ -0,0 +1,41 @@
+import { ChangeEvent, FormEvent, ReactElement, useState } from "react";
+import { Post } from "../common/types";
+import { useGitHub } from "../context/useGithub";
+
+import classes from "./login.module.css";
+
+interface LoginProps {
+  children: (posts: Post[]) => ReactElement<string, any>;
+}
+
+export function Login({ children }: LoginProps) {
+  const [userName, setUserName] = useState("");
+
+  const { user, onLogin, posts } = useGitHub();
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    if (userName !== "") {
+      onLogin(userName);
+    }
+  };
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setUserName(event.target.value);
+  };
+
+  if (!user) {
+    return (
+      <div className={classes.loginContainer}>
+        <form onSubmit={handleSubmit} className={classes.loginPapper}>
+          <label>Entre com seu usuário do GitHub</label>
+          <input value={userName} onChange={handleInputChange} />
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    );
+  }
+
+  return <>{children(posts)}</>;
+}
