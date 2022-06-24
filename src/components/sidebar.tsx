fa6c30e023d5bcf70333d30deba230137d0597db
Diogo Izele <diogogabrielizele@gmail.com>
2022-06-24 15:16:53 -0300

    refactor(sidebar): refactor sidebar to receive data of who is logged

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,23 +1,29 @@
 import classes from "./sidebar.module.css";
 import { PencilLine } from "phosphor-react";
 import { Avatar } from "./avatar";
+import { useGitHub } from "../context/useGithub";
 
 export function Sidebar() {
+  const { user } = useGitHub();
+
   return (
     <aside className={classes.sidebar}>
       <img
         className={classes.cover}
-        src="https://images.unsplash.com/photo-1553272725-086100aecf5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+        src="https://images.unsplash.com/photo-1484589065579-248aad0d8b13?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=459&q=80"
       />
 
       <div className={classes.profile}>
-        <Avatar src="https://github.com/diogoizele.png" />
-        <strong>Diogo Izele</strong>
-        <span>Web Developer</span>
+        <Avatar
+          src={user?.avatar_url!}
+          alt={`Foto de perfil de ${user?.name}`}
+        />
+        <strong>{user?.name ?? user?.login}</strong>
+        <span>{user?.bio}</span>
       </div>
 
       <footer>
-        <a href="#">
+        <a href={user?.html_url} target="_blank">
           <PencilLine size={20} />
           Editar seu perfil
         </a>
