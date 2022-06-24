f0c4e91ebaf33af4d44742ccbc54701cb18d9c4c
Diogo Izele <diogogabrielizele@gmail.com>
2022-06-24 10:18:18 -0300

    refactor(avatar): makes a drilling native props in avatar

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,15 +1,16 @@
+import { ImgHTMLAttributes } from "react";
 import classes from "./avatar.module.css";
 
-interface AvatarProps {
-  src: string;
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
   isComment?: boolean;
 }
 
-export function Avatar({ src, isComment }: AvatarProps) {
+export function Avatar({ src, isComment, ...nativeImgProps }: AvatarProps) {
   return (
     <img
       className={!isComment ? classes.avatar : classes.avatarComment}
       src={src}
+      {...nativeImgProps}
     />
   );
 }
