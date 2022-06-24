640529c6afbcf5b42e8807c2bca2d9725e68dbc0
Diogo Izele <diogogabrielizele@gmail.com>
2022-06-24 15:14:11 -0300

    feat(types): creates some general types

--- src/common/types.ts
+++ src/common/types.ts
@@ -8,3 +8,28 @@ export interface Content {
   text: string;
   type: "link" | "paragraph" | "hashtag";
 }
+
+export interface User {
+  avatar_url: string | null;
+  bio: string | null;
+  blog: string | null;
+  company: string | null;
+  followers: number;
+  following: number;
+  html_url: string;
+  login: string;
+  name: string;
+}
+
+export interface Post {
+  publishedAt: Date;
+  id: number;
+  author: Author;
+  content: Content[];
+}
+
+export interface Follower {
+  login: string
+  html_url: string
+  avatar_url: string
+}
